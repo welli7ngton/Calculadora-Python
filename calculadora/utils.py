a8a8949895cec388d2a3935f19b525dfb6797078
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-07-16 16:35:18 -0300

    ...

--- calculadora/utils.py
+++ calculadora/utils.py
@@ -7,5 +7,16 @@ def isNumOrDot(string: str):
     return bool(NUM_OR_DOT_REGEX.search(string))
 
 
+def isValidNumber(string: str):
+    valido = False
+    try:
+        float(string)
+        valido = True
+
+    except ValueError:
+        valido = False
+    return valido
+
+
 def isEmpty(string: str):
     return len(string) == 0
