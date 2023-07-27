b872d70abe2b9a0cda10823646c2728b692d25eb
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-07-27 17:02:50 -0300

    criando e emitindo sinais para as teclas digitadas

--- calculadora/display.py
+++ calculadora/display.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QLineEdit
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Signal
+from PySide6.QtGui import QKeyEvent
 from variaveis import BIG_FONT, MARGEM, LARGURA_MINIMA
+from utils import isEmpty, isNumOrDot
 
 
 # função para criar o dislay do histórico da calculadora
@@ -16,6 +18,11 @@ def cria_infor_display():
 
 
 class Display(QLineEdit):
+    igual = Signal()
+    backspc = Signal()
+    esc_p = Signal()
+    numeros = Signal(str)
+    operadores = Signal(str)
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
@@ -25,3 +32,35 @@ class Display(QLineEdit):
         self.setAlignment(Qt.AlignmentFlag.AlignRight)
         self.setTextMargins(*[MARGEM for _ in range(4)])
         self.setMinimumWidth(LARGURA_MINIMA)
+
+    def keyPressEvent(self, evento: QKeyEvent) -> None:
+        tecla = evento.key()
+        nome_tecla = evento.text().strip()
+
+        if tecla in [Qt.Key.Key_Enter, Qt.Key.Key_Return, Qt.Key.Key_Equal]:
+            print("enter pressionado, sinal emitido.")
+            self.igual.emit()
+            return evento.ignore()
+
+        if tecla in [Qt.Key.Key_Backspace, Qt.Key.Key_Delete, Qt.Key.Key_D]:
+            self.backspc.emit()
+            return evento.ignore()
+
+        if tecla in [Qt.Key.Key_Escape, Qt.Key.Key_C]:
+            self.esc_p.emit()
+            return evento.ignore()
+        if tecla in [
+            Qt.Key.Key_Equal, Qt.Key.Key_Minus, Qt.Key.Key_P, Qt.Key.Key_Plus,
+            Qt.Key.Key_Slash, Qt.Key.Key_multiply, Qt.Key.Key_Asterisk,
+        ]:
+            if nome_tecla.lower() == "p":
+                nome_tecla = "^"
+            self.operadores.emit(nome_tecla)
+            return evento.ignore()
+
+        if isEmpty(nome_tecla):
+            return evento.ignore()
+
+        if isNumOrDot(nome_tecla):
+            self.numeros.emit(nome_tecla)
+            return evento.ignore()
